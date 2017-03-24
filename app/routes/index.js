92fd8381e1c0310e16404f1ab846216c31c91532
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-03-24 08:58:14 -0700

    begin project, add routes for question and index and models for question and answer

---
+++ app/routes/index.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+    questions: this.store.findAll('question'),
+    answers: this.store.findAll('answer')
+  });
+  },
+  actions: {
+    destroyRental(rental) {
+      rental.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
+});
