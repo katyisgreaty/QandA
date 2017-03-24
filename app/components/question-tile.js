18116fd1fad442eed853390f186d70aeb0836351
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-03-24 10:07:54 -0700

    add update question functionality to index page

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -2,12 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   areNotesShowing: false,
+  updateQuestionForm: false,
   actions: {
     notesShow: function() {
       this.set('areNotesShowing', true);
     },
     notesHide: function() {
       this.set('areNotesShowing', false);
+    },
+    update(question, params) {
+      this.sendAction('update', question, params);
     },
     delete(question) {
       if (confirm('Are you sure you want to delete this question?')) {
